d2837f741098ad1ce8f301fcb0f23f5bf5e0ee98
= <=>
2024-08-10 12:28:33 +0900

    linux向けビルドスクリプト追加

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PACKAGE_NAME="GoldenBot"
+MAIN_SCRIPT="src/main.py"
+
+pyinstaller --onefile --name "$PACKAGE_NAME" "$MAIN_SCRIPT" --icon "images/favicon.ico"
+
+mkdir -p dist/config
+mkdir -p dist/log
+
+if [ ! -f "dist/config/config.json" ]; then
+    cp src/config/config.json dist/config/
+fi
+
+rm -rf build
